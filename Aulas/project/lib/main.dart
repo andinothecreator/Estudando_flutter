0556260307ebd258d7054d767f38842277118f53
Andinhoo-kiddo <andersoncabraldev1994@gmail.com>
2023-07-24 18:46:27 -0300

    Scaffold

--- Aulas/project/lib/main.dart
+++ Aulas/project/lib/main.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          'Eae vey, blz!',
-          style: TextStyle(fontSize: 45, color: Colors.blueAccent),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplicativo pra tirar 10"),
+        ),
+        body: Center(
+          child: Text(
+            'Eae vey, blz!',
+            style: TextStyle(fontSize: 45, color: Colors.blueAccent),
+          ),
         ),
       ),
     ),
