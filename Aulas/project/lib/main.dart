26321fc90e29b8ba28713ebe2ecc60b15b1abc73
Andinhoo-kiddo <acsj3@aluno.ifal.edu.br>
2023-07-26 18:04:07 +0000

    4 - STATELESS WIDGET e WIDGETS

--- Aulas/project/lib/main.dart
+++ Aulas/project/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(MyApp());
+  /*
   runApp(
     MaterialApp(
       home: Scaffold(
@@ -16,4 +18,26 @@ void main() {
       ),
     ),
   );
+  */
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplicativo pra tirar 10"),
+        ),
+        body: Center(
+          child: Text(
+            'Eae vey, blz!',
+            style: TextStyle(fontSize: 45, color: Colors.blueAccent),
+          ),
+        ),
+      ),
+    );
+  }
 }
